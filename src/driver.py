a544a6606d6b55c82ea0ee0cb290a005045ab086
Andrea Bajcsy <andrea.victoria.b@gmail.com>
2016-10-22 12:45:24 -0700

    beginning to extend for dynamics

--- src/driver.py
+++ src/driver.py
@@ -20,27 +20,8 @@ def get_linear_traj(start_x, start_y, goal_x, goal_y, waypoints):
 		traj[1][i]=traj[1][i-1]+dist_y/(waypoints-1)
 	
 	return traj
-
-if __name__ == '__main__':
-	num_traj_points = 30
 	
-	lambd = 0.2
-	other_weight = 0.1
-	
-	iter = 150	
-	epsilon = 60
-
-	# world params
-	N = 151
-	SX = 10
-	SY = 10
-	GX = 140
-	GY = 140
-	# define pt-based obstacles 
-	OBST = np.array([[50,60],[100,70]])
-
-	world = np.zeros((N,N))
-	
+def run_planner(num_traj_points, lambd, other_weight, iter, epsilon, N, SX, SY, GX, GY, OBST, world):
 	# set each pt obstacle into the world
 	for obj in OBST:
 		i = obj[0]
@@ -74,3 +55,40 @@ if __name__ == '__main__':
 		plt.plot(traj_progress[2*i+1,:], traj_progress[2*i+2,:],'g')
 		
 	plt.show()
+
+if __name__ == '__main__':
+	#setup the world and relevant parameters
+	num_traj_points = 30
+	
+	lambd = 0.2
+	other_weight = 0.1
+	
+	iter = 150	
+	epsilon = 60
+
+	# world params
+	N = 151
+	SX = 10
+	SY = 10
+	GX = 140
+	GY = 140
+	# define pt-based obstacles 
+	OBST = np.array([[50,60],[51,60],[50,61],[51,61],[100,70]])
+	world = np.zeros((N,N))
+	
+	# do initial trajectory planning
+	run_planner(num_traj_points, lambd, other_weight, iter, epsilon, N, SX, SY, GX, GY, OBST, world)
+	
+	# start to execute trajectory
+	
+	# at time t, get an interferance/force from human at location (x,y)
+	# add new obstacle at point where person interfered
+	human_obs = np.array([75,65])
+	OBST = np.vstack((OBST, human_obs))
+	SX = 40
+	SY = 75
+	
+	# replan trajectory from the point of disturbance, given now input
+	run_planner(num_traj_points, lambd, other_weight, iter, epsilon, N, SX, SY, GX, GY, OBST, world)
+	
+	
