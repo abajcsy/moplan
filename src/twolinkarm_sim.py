318bc1d0319bf8db6d561f3147c680eaca894465
Andrea Bajcsy <andrea.victoria.b@gmail.com>
2016-11-08 00:15:54 -0800

    cleaned some stuff up. now need to implement torque control

--- src/twolinkarm_sim.py
+++ src/twolinkarm_sim.py
@@ -15,91 +15,85 @@ from math import  *
 # the joint you want to affect. 
 #--------------------------------------------------#
 
-if __name__ == "__main__":
+class Arm():
 	# Constants
 	l1 = 1 			# link 1 length 
 	l2 = 1 			# link 2 length
-	m1 = 1 			# link 1 mass 
+	m1 = 1 		# link 1 mass 
 	m2 = 0.5 		# link 2 mass
-	dt = 0.01 		# sim time step
 
 	# Init Conditions
 	theta1 = pi/8 	# shoulder theta
 	theta2 = pi/8	# joint theta
 	dtheta1 = 0		# shoulder angular velocity 
 	dtheta2 = 0		# joint angular velocity 
-
-	fig = plt.figure(figsize=(10,10))
-	plt.clf()
-	ax = fig.add_subplot(111, aspect='equal', autoscale_on=False, xlim=(-1, 1), ylim=(-1, 1))
-	ax.grid()
-	plt.hold(True)
-
-	# initialize UserClick
-	user_click = UserClick(0, 0)
-
-	t = 0
+	
 	tau1 = 0 # torque on link1 (shoulder)
 	tau2 = 0 # torque on line2 (elbow)
 
 	r1 = 0.5 # distance from first joint to center of mass for link 1
 	r2 = 0.5 # distance from second joint to center of mass for link 2
+	
+	dt = 0.01 		# sim time step
 
 	# compute constants 
 	alpha = m1*r1**2 + m2*(l1**2 + r2**2)
 	beta = m2*l1*r2
 	delta = m2*r2**2
+	
+	# compute x,y position of arm
+	x1 = r1*cos(theta1)
+	y1 = r1*sin(theta1)
 
-	i = 0
-	while(1 is 1):
-		print "\n(tau1, tau2): ", (tau1, tau2)
-		
-		# looping until user quits 
-		t += dt
-		
-		c2 = cos(theta2)
-		s2 = sin(theta2)
+	x2 = x1 + r2*cos(theta1 + theta2) 
+	y2 = y1 + r2*sin(theta1 + theta2)
+
+	#def __init__(self):
+	
+	def update(self):
+		c2 = cos(self.theta2)
+		s2 = sin(self.theta2)
 		
 		# compute inertial forces due to acceleration of joints
-		M00 = alpha + 2*beta*c2
-		M01 = delta + beta*c2
-		M10 = delta + beta*c2
-		M11 = delta
+		M00 = self.alpha + 2*self.beta*c2
+		M01 = self.delta + self.beta*c2
+		M10 = self.delta + self.beta*c2
+		M11 = self.delta
 		
 		# compute coriolis and centrifugal forces
-		C00 = -beta*s2*dtheta1 
-		C01 = -beta*s2*(dtheta1 + dtheta2)
-		C10 = beta*s2*dtheta1 
+		C00 = -self.beta*s2*self.dtheta1 
+		C01 = -self.beta*s2*(self.dtheta1 + self.dtheta2)
+		C10 = self.beta*s2*self.dtheta1 
 		C11 = 0
 		
-		H1 = -beta*s2*dtheta1*dtheta2 - (1/2)*beta*s2*(dtheta2**2)
-		H2 = (1/2)*beta*s2*(dtheta1**2)
+		H1 = -self.beta*s2*self.dtheta1*self.dtheta2 - (1/2)*self.beta*s2*(self.dtheta2**2)
+		H2 = (1/2)*self.beta*s2*(self.dtheta1**2)
 		
 		# compute ddtheta's from tau1,tau2, and the inertial/corioilis forces
-		ddtheta2 = (H2*M00 - H1*M10 -M00*tau2 + M10*tau1) / (M01**2 - M00*M11)
-		ddtheta1 = (-H2 + tau2 - M11*ddtheta2) / M10
+		ddtheta2 = (H2*M00 - H1*M10 -M00*self.tau2 + M10*self.tau1) / (M01**2 - M00*M11)
+		ddtheta1 = (-H2 + self.tau2 - M11*ddtheta2) / M10
 
 		print "ddtheta1: ", ddtheta1
 		print "ddtheta2: ", ddtheta2
 		
 		# update dtheta and theta
-		dtheta1_new = dtheta1 + ddtheta1*dt
-		dtheta2_new = dtheta2 + ddtheta2*dt
-		theta1_new = theta1 + dtheta1_new*dt
-		theta2_new = theta2 + dtheta2_new*dt
+		dtheta1_new = self.dtheta1 + ddtheta1*self.dt
+		dtheta2_new = self.dtheta2 + ddtheta2*self.dt
+		theta1_new = self.theta1 + dtheta1_new*self.dt
+		theta2_new = self.theta2 + dtheta2_new*self.dt
 		
 		# move to new timestep
-		dtheta1 = dtheta1_new
-		dtheta2 = dtheta2_new
+		self.dtheta1 = dtheta1_new
+		self.dtheta2 = dtheta2_new
 		
-		print "dtheta1: ", dtheta1
-		print "dtheta2: ", dtheta2
+		print "dtheta1: ", self.dtheta1
+		print "dtheta2: ", self.dtheta2
 		
-		theta1 = theta1_new
-		theta2 = theta2_new
+		self.theta1 = theta1_new
+		self.theta2 = theta2_new
 		
-		print "theta1: ", theta1
-		print "theta2: ", theta2
+		print "theta1: ", self.theta1
+		print "theta2: ", self.theta2
 		
 		# enforce joint constraints 
 		if (1 == 2): #just for testing now
@@ -120,62 +114,79 @@ if __name__ == "__main__":
 				theta2 = pi-0.1
 				dtheta2 = 0
 				ddtheta2 = 0 
-		
+				
 		# compute new x,y position of arm
-		x1 = r1*cos(theta1)
-		y1 = r1*sin(theta1)
+		self.x1 = self.r1*cos(self.theta1)
+		self.y1 = self.r1*sin(self.theta1)
 
-		x2 = x1 + r2*cos(theta1 + theta2) 
-		y2 = y1 + r2*sin(theta1 + theta2)
+		self.x2 = self.x1 + self.r2*cos(self.theta1 + self.theta2) 
+		self.y2 = self.y1 + self.r2*sin(self.theta1 + self.theta2)
 		
-		print "(x1,y1): ", (x1, y1)
-		print "(x2,y2): ", (x2, y2)
-			
-		cid = fig.canvas.mpl_connect('button_press_event', user_click)
-		print "new_click: ", user_click.new_click
+		print "(x1,y1): ", (self.x1, self.y1)
+		print "(x2,y2): ", (self.x2, self.y2)
 
-		if(user_click.new_click):
-			user_x = user_click.x
-			user_y = user_click.y
-			
-			# distance to midpoint of l1 and l2
-			dist_to_l1 = math.hypot(user_x-(x1/2), user_y-(y1/2))
-			dist_to_l2 = math.hypot(user_x-((x2+x1)/2), user_y-((y2+y1)/2))
-			
-			if dist_to_l1 < dist_to_l2: # user interacting with shoulder
-				value_l1 = (x1-0)*(user_y-0) - (user_x - 0)*(y1 - 0)
-				if value_l1 > 0:
-					print "SHOULDER torque on LEFT"
-					# torque applied from left 
-					tau1 -= 0.1
-				else:
-					print "SHOULDER torque on RIGHT"
-					# torque applied from right
-					tau1 += 0.1
-			else: # user interacting with elbow
-				value_l2 = (x2-x1)*(user_y-x1) - (user_x - x1)*(y2 - y1)
-				if value_l2 > 0:
-					print "ELBOW torque on LEFT"
-					# torque applied from left 
-					tau2 -= 0.1
-				else:
-					print "ELBOW torque on RIGHT"
-					# torque applied from right
-					tau2 += 0.1
-			user_click.new_click = False
+
+def get_line_traj((SX,SY), (GX,GY), num_waypoints):
+	traj = np.zeros((2,num_waypoints))
+	traj[0][0] = SX
+	traj[1][0] = SY
+	dist_x = GX-SX
+	dist_y = GY-SY
+
+	for i in range(1,num_waypoints):
+		traj[0][i] = traj[0][i-1] + dist_x/(num_waypoints-1)
+		traj[1][i] = traj[1][i-1] + dist_y/(num_waypoints-1)
 		
+	return traj
+
+
+if __name__ == "__main__":
+
+	arm = Arm()
+	
+	fig = plt.figure(figsize=(10,10))
+	plt.clf()
+	ax = fig.add_subplot(111, aspect='equal', autoscale_on=False, xlim=(-1, 1), ylim=(-1, 1))
+	ax.grid()
+	plt.hold(True)
+
+	# generate trajectory
+	SX = 0.9
+	SY = 0.5
+	GX = -0.9
+	GY = 0.5
+	num_waypoints = 10
+	traj = get_line_traj((SX,SY), (GX,GY), num_waypoints)
+	plt.plot(traj[0],traj[1],'ro')
+	
+	t = 0
+	i = 0
+	while(1 is 1):
+		print "\n(tau1, tau2): ", (arm.tau1, arm.tau2)
+		
+		# looping until user quits 
+		t += arm.dt
+		
+		# update arm position given changes in it's state
+		arm.update()
+		
+		# simulate applied torque 
+		arm.tau1 += 0.1
+			
 		plt.clf()
-		plt.hold(True)
 		ax = fig.add_subplot(111, aspect='equal', autoscale_on=False, xlim=(-1, 1), ylim=(-1, 1))
 		ax.grid()
 		
-		plt.plot([0, x1], [0, y1], 'b', linewidth=4)
-		plt.plot([x1, x2], [y1, y2], 'b', linewidth=4)
+		plt.plot([0, arm.x1], [0, arm.y1], 'b', linewidth=5)
+		plt.plot([arm.x1, arm.x2], [arm.y1, arm.y2], 'b', linewidth=5)
 		
 		# plot joints
 		plt.plot(0,0,'ko')
-		plt.plot(x1,y1,'ko'); 
-		plt.plot(x2,y2,'ko') 
+		plt.plot(arm.x1, arm.y1, 'ko') 
+		plt.plot(arm.x2, arm.y2, 'ko')
+		
+		# plot trajectory waypoints
+		plt.plot(traj[0],traj[1],'ro')
 		
 		#plt.show(block=False)
 		plt.pause(0.001)
